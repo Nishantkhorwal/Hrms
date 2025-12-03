f9e3f2e4db1ced27c2c4b17c2a90a0fbb1f8d837
NishantKhorwal <khorwalnishant@gmail.com>
2025-12-03 17:28:15 +0530

    a

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -35,7 +35,7 @@ export default function LoginPage() {
         headers: {
           "Content-Type": "application/json",
         },
-        credentials: "include",
+      
         body: JSON.stringify(formData),
       })
 
